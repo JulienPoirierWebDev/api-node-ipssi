1437ab86c4d5f77e83212e1647fde1c4f320c50c
jupo <jupo@debian.jupowebdev>
2026-03-16 16:35:36 +0100

    CRUD in memory done

--- src/server.ts
+++ src/server.ts
@@ -49,10 +49,17 @@ export type CharacterType = {
   updatedAd: Date;
 };
 
-const characters: CharacterType[] = [];
+let characters: CharacterType[] = [];
 
 app.post('/characters', (request, response) => {
   const body = request.body;
+
+  if (!body.name || !body.description) {
+    // TODO il faut aussi vérifier le type des données !
+    return response
+      .status(400)
+      .json({ message: 'Il manque des données', error: true });
+  }
 
   const newCharacter: CharacterType = {
     id: Date.now(),
@@ -92,6 +99,84 @@ En modifier un avec son ID
 En supprimer un avec son ID
 
 */
+
+app.get('/characters/:id', (request, response) => {
+  const id = Number(request.params.id);
+
+  if (isNaN(id)) {
+    return response
+      .status(400)
+      .json({ message: "L'id n'est pas un nombre", error: true });
+  }
+
+  const character = characters.find((oneCharacter) => oneCharacter.id === id);
+
+  if (!character) {
+    return response
+      .status(404)
+      .json({ message: "Il n'y a pas de perso avec cet ID", error: true });
+  }
+
+  response.json({ message: 'Perso trouvé', result: character });
+});
+
+app.put('/characters/:id', (request, response) => {
+  const id = Number(request.params.id);
+
+  if (isNaN(id)) {
+    return response
+      .status(400)
+      .json({ message: "L'id n'est pas un nombre", error: true });
+  }
+
+  const character = characters.find((oneCharacter) => oneCharacter.id === id);
+
+  if (!character) {
+    return response
+      .status(404)
+      .json({ message: "Il n'y a pas de perso avec cet ID", error: true });
+  }
+
+  //const body = request.body;
+  const { name, description } = request.body;
+
+  character.name = name;
+  character.description = description;
+  character.updatedAd = new Date();
+
+  response.json({ message: 'Perso modifié', result: character });
+});
+
+app.delete('/characters/:id', (request, response) => {
+  const id = Number(request.params.id);
+
+  if (isNaN(id)) {
+    return response
+      .status(400)
+      .json({ message: "L'id n'est pas un nombre", error: true });
+  }
+
+  //      [a, b, c, d]
+  // index 0  1  2  3
+
+  //Si on veut supprimer c : il faut déja trouver son index.
+  const characterIndex = characters.findIndex(
+    (oneCharacter) => oneCharacter.id === id,
+  );
+
+  console.log(characterIndex);
+
+  if (characterIndex === -1) {
+    return response
+      .status(404)
+      .json({ message: "Il n'y a pas de perso avec cet ID", error: true });
+  }
+
+  // supprimer l'élement qui a l'index X
+  characters.splice(characterIndex, 1); // Le deuxième paramètre signifie qu'il faut supprimer un seul élément
+
+  response.json({ message: 'Perso supprimé' });
+});
 
 // L'application doit écouter sur un port pour fonctionner.
 app.listen(3000, () => {
