67a25ab2922b801f769434b4e138ac004bea8552
jupo <jupo@debian.jupowebdev>
2026-03-16 11:15:26 +0100

    first route

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,12 @@
-console.log("coucou");
+import express from "express";
 
+const app = express()
+
+app.get("/", (request, response) => {
+    response.json({message:"Tout est OK sur la route principale"})
+})
+
+app.listen(3000, () => {
+  console.log("Server en fonctionnement");
+
+})
