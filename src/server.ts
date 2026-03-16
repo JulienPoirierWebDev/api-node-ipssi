0e0b19e20db615d8776abba79dbb939879fa2644
jupo <jupo@debian.jupowebdev>
2026-03-16 14:57:21 +0100

    add prettier config

--- src/server.ts
+++ src/server.ts
@@ -1,80 +1,84 @@
-import express from "express";
+import express from 'express';
 
 //on prépare une application vide (pour le moment) app est une instance d'express
-const app = express()
+const app = express();
 
+// Middleware global qui permet d'acceder au body de la requête reçue.
 app.use(express.json());
 
 // On enregistre une route : méthode GET sur endpoint (chemin) "/"
 // Une fonction callback (appelé plus tard / après) est associée au combo méthode + endpoint
 // Quand l'app écoute une requête sur "/" avec la méthode GET, alors elle exécuté la fonction
-app.get("/", (request, response) => {
+app.get('/', (request, response) => {
   // request qui va contenir toutes les infos sur la requete reçue
   // (les infos du client, la route, les query parameters, etc)
   // response est la reponse qui sera envoyé par notre API vers le client : nous devons l'enrichir
   // pour que le client reçoive ce que l'on souhaite.
-    response.json({message:"Tout est OK sur la route principale"})
-})
+  response.json({ message: 'Tout est OK sur la route principale' });
+});
 
-app.get("/date", (request, response) => {
+app.get('/date', (request, response) => {
   const date = new Date();
-  response.json({message : `Nous sommes le ${date.toLocaleDateString()} `})
-})
+  response.json({ message: `Nous sommes le ${date.toLocaleDateString()} ` });
+});
 
-app.get("/questions", async (request, response) => {
+app.get('/questions', async (request, response) => {
   try {
-    const fetchRequest = await fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy")
-    if(fetchRequest.ok) {
+    const fetchRequest = await fetch(
+      'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy',
+    );
+    if (fetchRequest.ok) {
       const data = await fetchRequest.json();
       response.json(data);
     } else {
-      throw new Error()
+      throw new Error();
     }
-
   } catch (error) {
-    response.status(503).json({message:"Il y a un problème chez nous", error:true})
+    response
+      .status(503)
+      .json({ message: 'Il y a un problème chez nous', error: true });
   }
-})
-
+});
 
 export type CharacterType = {
-  id:number,
-  name:string,
-  description:string,
-  imageUrl?:string,
-  createdAt:Date,
-  updatedAd:Date
-}
+  id: number;
+  name: string;
+  description: string;
+  imageUrl?: string;
+  createdAt: Date;
+  updatedAd: Date;
+};
 
-const characters:CharacterType[] = [];
+const characters: CharacterType[] = [];
 
 app.post('/characters', (request, response) => {
+  const body = request.body;
 
-    const body = request.body
+  const newCharacter: CharacterType = {
+    id: Date.now(),
+    name: body.name,
+    description: body.description,
+    createdAt: new Date(),
+    updatedAd: new Date(),
+  };
 
-    const newCharacter : CharacterType = {
-      id:Date.now(),
-      name:body.name,
-      description:body.description,
-      createdAt:new Date(),
-      updatedAd:new Date()
-  }
+  characters.push(newCharacter);
 
-
-
-  characters.push(newCharacter)
-
-  response.status(201).json({message:"Perso crée !", character : newCharacter})
-})
+  response
+    .status(201)
+    .json({ message: 'Perso crée !', character: newCharacter });
+});
 
 app.get('/characters', (request, response) => {
-  if(characters.length === 0) {
-    response.json({message:"Il n'y a aucun personnage en base ", results : null})
+  if (characters.length === 0) {
+    response.json({
+      message: "Il n'y a aucun personnage en base ",
+      results: null,
+    });
   } else {
-    response.json({message:"Données trouvées ", results : characters})
+    response.json({ message: 'Données trouvées ', results: characters });
   }
-})
-
+});
 
 /*
 TODO :
@@ -92,5 +96,5 @@ En supprimer un avec son ID
 // L'application doit écouter sur un port pour fonctionner.
 app.listen(3000, () => {
   // Message en cas de réussite
-  console.log("Server en fonctionnement");
-})
+  console.log('Server en fonctionnement');
+});
